4f6c8e27744bb733ad57a14a8d176356702d2c27
Sebastian Laskawiec <slaskawi@redhat.com>
2017-03-15 15:35:45 +0100

    JDG-852 Spring Session quickstart

---
+++ spring-session/src/main/java/org/jboss/as/quickstarts/datagrid/spring/session/configuration/SessionsController.java
@@ -0,0 +1,28 @@
+package org.jboss.as.quickstarts.datagrid.spring.session.configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.infinispan.spring.provider.SpringEmbeddedCacheManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SessionsController {
+
+   @Autowired
+   SpringEmbeddedCacheManager cacheManager;
+
+   @RequestMapping("/sessions")
+   public Map<String, String> session(HttpServletRequest request) {
+      Map<String, String> result = new HashMap<>();
+      String sessionId = request.getSession(true).getId();
+      result.put("created:", sessionId);
+      // By default Infinispan integration for Spring Session will use 'sessions' cache.
+      result.put("active:", cacheManager.getCache("sessions").getNativeCache().keySet().toString());
+      return result;
+   }
+}
