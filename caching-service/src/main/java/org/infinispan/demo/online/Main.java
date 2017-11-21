54ce261332ad53be4baa03c49338374f34b69b41
Ryan Emerson <remerson@redhat.com>
2017-11-21 13:19:03 +0000

    Authentication added to caching-service quickstart

--- caching-service/src/main/java/org/infinispan/demo/online/Main.java
+++ caching-service/src/main/java/org/infinispan/demo/online/Main.java
@@ -7,19 +7,34 @@ import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.Configuration;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.client.hotrod.configuration.SaslQop;
 
 public class Main {
 
-   /**
-    * Hot Rod endpoint is constructed using the following scheme:
-    *   `application name`-hotrod.
-    */
-   private static final String HOT_ROD_ENDPOINT_SERVICE = "caching-service-app-hotrod";
+   // This should match the value specified for the APPLICATION_NAME parameter when creating the caching-service
+   private static final String APPLICATION_NAME = "caching-service-app";
+
+   // Hot Rod endpoint is constructed using the following scheme: `application name`-hotrod.
+   private static final String HOT_ROD_ENDPOINT_SERVICE = APPLICATION_NAME + "-hotrod";
+
+   // This should match the value specified for the APPLICATION_USER parameter when creating the caching-service
+   private static final String USERNAME = "test";
+
+   // This should match the value specified for the APPLICATION_USER_PASSWORD parameter when creating the caching-service
+   private static final String PASSWORD = "test";
+
 
    public static void main(String... args) throws InterruptedException {
       Configuration c = new ConfigurationBuilder()
             .addServer()
                 .host(HOT_ROD_ENDPOINT_SERVICE)
+                .security().authentication().enable()
+                  .username(USERNAME)
+                  .password(PASSWORD)
+                  .realm("ApplicationRealm")
+                  .serverName("caching-service")
+                  .saslMechanism("DIGEST-MD5")
+                  .saslQop(SaslQop.AUTH)
             .build();
 
       // When using topology and hash aware client (this is the default), the client
