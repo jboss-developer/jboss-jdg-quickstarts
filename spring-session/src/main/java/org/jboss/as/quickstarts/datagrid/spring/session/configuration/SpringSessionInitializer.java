3fe09065f1aad288aa117743358cc0025ebdd2c2
Sebastian Laskawiec <slaskawi@redhat.com>
2018-02-16 13:13:41 +0100

    Fixed typo

--- spring-session/src/main/java/org/jboss/as/quickstarts/datagrid/spring/session/configuration/SpringSessionInitializer.java
+++ spring-session/src/main/java/org/jboss/as/quickstarts/datagrid/spring/session/configuration/SpringSessionInitializer.java
@@ -5,7 +5,7 @@
 ///**
 // * This class has been intentionally commented out.
 // *
-// * When when one can not use Spring Boot (and uses Spring Beans/Context/Web configured manually),
+// * When one can not use Spring Boot (and uses Spring Beans/Context/Web configured manually),
 // * this class is essential to tell Spring Session how to hook into the container.
 // *
 // * Unfortunately adding it to Spring Boot-enabled deployment will cause a failure (since you can not have
