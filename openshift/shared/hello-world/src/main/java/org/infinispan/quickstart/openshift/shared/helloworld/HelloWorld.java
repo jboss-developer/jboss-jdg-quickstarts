62ad3da9b15d79152a8400f9c5ff95c4f4011d49
Galder Zamarreño <galder@zamarreno.com>
2018-11-29 12:51:28 +0100

    JDG-2259 OpenShift hello world tutorial is shared between services
    * Move the tutorial to a shared area where tutorials that are service
      independent can be located.

---
+++ openshift/shared/hello-world/src/main/java/org/infinispan/quickstart/openshift/shared/helloworld/HelloWorld.java
@@ -0,0 +1,36 @@
+package org.infinispan.quickstart.openshift.shared.helloworld;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+
+public class HelloWorld {
+
+   private static final String APP_NAME = System.getenv("APP_NAME");
+   private static final String HOST = APP_NAME + "-hotrod";
+   private static final String USER = "test";
+   private static final String PASSWORD = "changeme";
+
+   private static final String KEY = "hello";
+   private static final String VALUE = "world";
+
+   public static void main(String[] args) {
+      ConfigurationBuilder cfg =
+         ClientConfiguration.create(HOST, APP_NAME, USER, PASSWORD);
+
+      System.out.printf("--- Connect to %s ---%n", APP_NAME);
+      final RemoteCacheManager remote = new RemoteCacheManager(cfg.build());
+
+      System.out.printf("--- Store key='%s'/value='%s' pair ---%n", KEY, VALUE);
+      final RemoteCache<String, String> remoteCache = remote.getCache();
+      remoteCache.put(KEY, VALUE);
+
+      System.out.printf("--- Retrieve key='%s' ---%n", KEY);
+      final String value = remoteCache.get(KEY);
+
+      System.out.printf("--- Value is '%s' ---%n", value);
+
+      assert VALUE.equals(value) : "Expected value to be '" + value + "' but was: " + value;
+   }
+
+}
