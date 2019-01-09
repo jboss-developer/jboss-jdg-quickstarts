9d6863b22b30fbad69619c77f7f077f394ddd767
Diego Lovison <diegolovison@gmail.com>
2019-01-09 13:39:51 -0200

    Create spring5-session module based on spring4-session

---
+++ spring5-session/src/main/java/org/jboss/as/quickstarts/datagrid/spring/session/configuration/SpringSessionInitializer.java
@@ -0,0 +1,19 @@
+//package org.jboss.as.quickstarts.datagrid.spring.session.configuration;
+//
+//import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+//
+///**
+// * This class has been intentionally commented out.
+// *
+// * When one can not use Spring Boot (and uses Spring Beans/Context/Web configured manually),
+// * this class is essential to tell Spring Session how to hook into the container.
+// *
+// * Unfortunately adding it to Spring Boot-enabled deployment will cause a failure (since you can not have
+// * two <code>WebApplicationInitializer</code> instances in the same deployment.
+// */
+//public class SpringSessionInitializer extends AbstractHttpSessionApplicationInitializer {
+//
+//   public SpringSessionInitializer() {
+//      super(InfinispanConfiguration.class);
+//   }
+//}
