52732e00b94734ff638e2ca5ea3453258276e7ec
Galder Zamarreño <galder@zamarreno.com>
2019-01-15 14:30:15 +0100

    JDG-2258 Update external access tutorial to latest 7.3 image

--- openshift/shared/external-access/test.sh
+++ openshift/shared/external-access/test.sh
@@ -18,6 +18,7 @@ usage() {
 		-q --quickstart-only     only test the quickstart, it assumes the service is running.
 		-s --service-name        service to test, can be either cache-service or datagrid-service.
 		                         cache-service is default.
+		-i --image               optional parameter with image to test.
 		-x --debug               debug
 
 
@@ -37,6 +38,9 @@ usage() {
 		Clean and remove quickstart and datagrid-service deployment:
 		$PROGNAME --clean --service-name datagrid-service
 
+		Test quickstart on custom image:
+		$PROGNAME --image ...
+
 		Run with extra logging:
 		$PROGNAME --debug
 	EOF
@@ -53,6 +57,7 @@ cmdline() {
             --help)                    args="${args}-h ";;
             --quickstart-only)         args="${args}-q ";;
             --service-name)            args="${args}-s ";;
+            --image)                   args="${args}-i ";;
             --debug)                   args="${args}-x ";;
             #pass through anything else
             *) [[ "${arg:0:1}" == "-" ]] || delim="\""
@@ -63,7 +68,7 @@ cmdline() {
     #Reset the positional parameters to the short options
     eval set -- $args
 
-    while getopts "chqs:x" OPTION
+    while getopts "chqs:i:x" OPTION
     do
          case $OPTION in
          c)
@@ -78,6 +83,9 @@ cmdline() {
              ;;
          s)
              SERVICE_NAME=$OPTARG
+             ;;
+         i)
+             readonly IMAGE=$OPTARG
              ;;
          x)
              readonly DEBUG='-x'
@@ -107,14 +115,22 @@ startService() {
     local appName=$2
     echo "--> Start service from template '${svcName}' as '${appName}'"
 
-    # TODO last datagrid73-dev commit on 15.11.18
+    # TODO last datagrid73-dev commit on 11.01.19
     oc create -f \
-        https://raw.githubusercontent.com/jboss-container-images/jboss-datagrid-7-openshift-image/f186aba68a3042605e64daac706e7ca364b1c758/services/${svcName}-template.yaml
+        https://raw.githubusercontent.com/jboss-container-images/jboss-datagrid-7-openshift-image/e16ac0a0c8c972afc72d8709e1a9a75a75edea04/services/${svcName}-template.yaml
 
-    oc new-app ${svcName} \
-        -p APPLICATION_USER=test \
-        -p APPLICATION_USER_PASSWORD=changeme \
-        -p APPLICATION_NAME=${appName}
+    if [ -n "${IMAGE+1}" ]; then
+        oc new-app ${svcName} \
+            -p APPLICATION_USER=test \
+            -p APPLICATION_PASSWORD=changeme \
+            -p APPLICATION_NAME=${appName} \
+            -p IMAGE=${IMAGE}
+    else
+        oc new-app ${svcName} \
+            -p APPLICATION_USER=test \
+            -p APPLICATION_PASSWORD=changeme \
+            -p APPLICATION_NAME=${appName}
+    fi
 }
 
 
@@ -160,13 +176,12 @@ startQuickstart() {
 
 runHttpsQuickstart() {
     local appName=$1
+    local publicIp=$2
 
     echo "--> Run HTTPs quickstart for '${appName}'"
 
     rm -drf tls.crt
     oc get secret service-certs -o jsonpath='{.data.tls\.crt}' | base64 -D > tls.crt
-
-    local ip=$(minishift ip)
 
     local curlCmd="curl"
     if [ -n "${DEBUG+1}" ]; then
@@ -180,14 +195,27 @@ runHttpsQuickstart() {
         --cacert tls.crt \
         -H 'Content-type: text/plain' \
         -d 'mundo' \
-        https://${appName}-https-route-myproject.${ip}.nip.io/rest/default/hola
+        https://${appName}-https-route-myproject.${publicIp}.nip.io/rest/default/hola
 
     echo "--> Retrieve value via HTTPs:"
     ${curlCmd} -X GET \
         -k \
         -u test:changeme \
         --cacert tls.crt \
-        https://${appName}-https-route-myproject.${ip}.nip.io/rest/default/hola
+        https://${appName}-https-route-myproject.${publicIp}.nip.io/rest/default/hola
+}
+
+
+getIp() {
+    set +e
+    minishift ip
+    local result=$?
+    if [ ${result} -eq 0 ]; then
+        echo result
+    else
+        echo "127.0.0.1"
+    fi
+    set -e
 }
 
 
@@ -204,7 +232,8 @@ main() {
 
     echo "--> Test params: service=${svcName},app=${appName}";
 
-    oc login $(minishift ip):8443 -u developer -p developer
+    local publicIp=$(getIp)
+    oc login ${publicIp}:8443 -u developer -p developer
 
     if [ -n "${CLEAN+1}" ]; then
         stopService ${svcName} ${hotRodRouteName} ${httpsRouteName}
@@ -224,7 +253,7 @@ main() {
 
         startQuickstart ${appName}
 
-        runHttpsQuickstart ${appName}
+        runHttpsQuickstart ${appName} ${publicIp}
     fi
 
 }
